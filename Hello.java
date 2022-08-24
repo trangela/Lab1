c498ba79d8a8e2be5a648c8cc53ad8f1326af711
trangela <112021133+trangela@users.noreply.github.com>
2022-08-24 16:11:18 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+	public static void main(String[] args) {
+System.out.println("<Angela Tran>");
+System.out.println("<Java, macOs Monterey>");
+System.out.println("<Lab 1/Download Java>");
+System.out.println("<8/23/22>");
+System.out.println("<CMSC 255, 001>");
+System.out.println();
+	}	
+}
